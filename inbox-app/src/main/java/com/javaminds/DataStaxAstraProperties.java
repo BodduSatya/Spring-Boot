0a55bc2daf81b891ceda8964860fad7a1e55e918
Satya B <bvvsnc3@gmail.com>
2022-03-27 14:53:38 +0530

    add astra connection

---
+++ inbox-app/src/main/java/com/javaminds/DataStaxAstraProperties.java
@@ -0,0 +1,23 @@
+package com.javaminds;
+
+
+import java.io.File;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="datastax.astra")
+public class DataStaxAstraProperties {
+
+    private File secureConnectBundle;
+        
+    public void setSecureConnectBundle(File secureConnectBundle) {
+        this.secureConnectBundle = secureConnectBundle;
+    }
+
+    public File getSecureConnectBundle() {
+        return secureConnectBundle;
+    }
+
+    
+
+}
--- inbox-app/src/main/java/com/javaminds/InboxApp.java
+++ inbox-app/src/main/java/com/javaminds/InboxApp.java
@@ -1,13 +1,23 @@
 package com.javaminds;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InboxApp  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApp.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 }
