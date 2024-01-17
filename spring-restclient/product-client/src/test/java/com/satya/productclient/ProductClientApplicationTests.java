be57e92c25a8d0e4c9283f915baa908a97063db9
SatyaNarayana Boddu <bvvsnc3@gmail.com>
2024-01-17 17:56:04 +0530

    v1

---
+++ spring-restclient/product-client/src/test/java/com/satya/productclient/ProductClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satya.productclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-restclient/product-service/src/test/java/com/satya/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satya.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
