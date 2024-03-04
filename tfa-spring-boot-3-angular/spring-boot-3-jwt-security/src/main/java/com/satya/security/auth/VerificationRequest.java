425de4e8a53c283bf57b8ed47545f34f47f18687
SatyaNarayana Boddu <bvvsnc3@gmail.com>
2024-03-04 07:47:13 +0530

    updated

---
+++ tfa-spring-boot-3-angular/spring-boot-3-jwt-security/src/main/java/com/satya/security/auth/VerificationRequest.java
@@ -0,0 +1,12 @@
+package com.satya.security.auth;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class VerificationRequest {
+
+    private String email;
+    private String code;
+}
